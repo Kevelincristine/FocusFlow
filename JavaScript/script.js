e45f42f37b7ba206d96c114ea05754775913cff9
Kevelin Cristine Santos <kevelincristine82@gmail.com>
2025-12-26 13:34:35 -0300

    add js to my project

---
+++ JavaScript/script.js
@@ -0,0 +1,4 @@
+let timer = document.querySelector("#timer")
+let start  = document.querySelector("#start")
+let pause = document.querySelector("#pause")
+let reset = document.querySelector("#reset")

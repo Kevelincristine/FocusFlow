8c458719a0c34ebf7c072585512bd14883d8f6c7
Kevelin Cristine Santos <kevelincristine82@gmail.com>
2025-12-28 15:12:12 -0300

    js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,3 +2,14 @@ let timer = document.querySelector("#timer")
 let start  = document.querySelector("#start")
 let pause = document.querySelector("#pause")
 let reset = document.querySelector("#reset")
+let totalSeconds = 25 * 60
+let interval = null
+function updateTimer(){
+    const minutes = Math.floor( totalSeconds / 60)
+    const seconds = totalSeconds % 60
+    document.getElementById("timer").innerHTML =
+    `${String(minutes).padStart(2, "0")}:{$String(seconds).padStart(2, "0")}`
+}
+function startTimer(){
+    if (interval) return 
+}

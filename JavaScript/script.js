9818f58721f9a04ce82b658a5d2c3c3dd09bfb65
Kevelin Cristine Santos <kevelincristine82@gmail.com>
2025-12-28 16:45:34 -0300

    add pomoro timer and todo list

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,15 +1,72 @@
-let timer = document.querySelector("#timer")
-let start  = document.querySelector("#start")
-let pause = document.querySelector("#pause")
-let reset = document.querySelector("#reset")
-let totalSeconds = 25 * 60
-let interval = null
+let isBreak = false;
+let time = 25 *60 ;
+let timer;
+const display = document.querySelector("#timer");
+const startBtn = document.querySelector("#start");
+const resetBtn = document.querySelector("#reset");
+const pauseBtn = document.querySelector("#pause")
+
+const input = document.querySelector("#todo-input");
+const addBtn = document.querySelector("#addTask");
+const list = document.querySelector("#todo-list")
+
 function updateTimer(){
-    const minutes = Math.floor( totalSeconds / 60)
-    const seconds = totalSeconds % 60
-    document.getElementById("timer").innerHTML =
-    `${String(minutes).padStart(2, "0")}:{$String(seconds).padStart(2, "0")}`
+    const minutes = Math.floor(time/60);
+    const seconds = time % 60;
+
+    display.textContent = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+
+
 }
-function startTimer(){
-    if (interval) return 
-}
+ startBtn.addEventListener("click",() => {
+    if (timer) return;
+    timer = setInterval (() => {
+        if (time > 0 ) {
+            time--;
+            updateTimer();
+        } else {
+            clearInterval(timer);
+            timer = null;
+            if (!isBreak) {
+                isBreak = true;
+                time = 5 * 60;
+                updateTimer();
+                startBtn.click();
+            }
+            else {
+                isBreak = false;
+                time = 25 * 60;
+                updateTimer();
+                alert("Pomodoro Finished!")
+            }
+        }
+    } , 1000 )
+ });
+ pauseBtn.addEventListener("click", () =>{
+    if (!timer) return;
+    
+    clearInterval(timer);
+    timer = null;
+ });
+ resetBtn.addEventListener("click",()=>{
+    clearInterval(timer);
+    timer = null;
+    isBreak = false;
+    time = 25 * 60;
+    updateTimer();
+ });
+ 
+addBtn.addEventListener("click",
+    () => {
+        if (input.value === "") return;
+    const li = document.createElement("li");
+    li.textContent = input.value;
+
+    li.addEventListener("click", () =>{
+        li.remove();
+    });
+    list.appendChild(li);
+    input.value = "";
+    }
+)
+ 
